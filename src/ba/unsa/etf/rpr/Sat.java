c8d83241c5f11d5d23063a9c6b688f7e53ee1ffd
Kemal Hadžiabdić <khadziabdi1@etf.unsa.ba>
2021-10-18 12:40:09 +0200

    Init commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    int sati, minute,sekunde;
+    public Sat(int noviSati,int noveMinute,int noveSekunde){
+        sati=noviSati;
+        sekunde=noveSekunde;
+        minute=noveMinute;
+    }
+    public void postavi(int noviSati,int noveMinute,int noveSekunde){
+        sati=noviSati;
+        sekunde=noveSekunde;
+        minute=noveMinute;
+    }
+    public void sljedeci(){
+        sekunde++;
+        if (sekunde==60) { sekunde=0; minute++; }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
+    public void prethodni(){
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+    public void pomjeriZa(int pomak){
+        if (pomak>0) {
+            for (int i = 0; i < pomak ; i++) sljedeci();
+        }
+		else {
+            for (int i = 0; i < -pomak ; i++) prethodni();
+        }
+    }
+    public int dajSate(){return sati;}
+    public int dajMinute(){return minute;}
+    public int dajSekunde(){return sekunde;}
+    public void ispisi(){
+        System.out.printf("%d:%d:%d\n",sati,minute,sekunde);
+    }
+}
